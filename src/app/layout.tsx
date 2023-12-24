6993fc6e33e958f02630f875a3ac543811cf8e53
ashsajal1 <ashsajal1@gmail.com>
2023-12-24 17:00:34 +0800

    Edit cover

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,28 +8,28 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'AI : Question answering',
-  description: 'Ai tools to answer question of specific context! Api is provided by Huggingface.',
-  // openGraph: {
-  //   title: 'AI : Question answering',
-  //   description: 'Ai tools to answer question of specific context! Api is provided by Huggingface.',
-  //   url: 'https://ai-qna-six.vercel.app',
-  //   siteName: 'Next.js',
-  //   images: [
-  //     {
-  //       url: '/opengraph-image.jpg',
-  //       width: 800,
-  //       height: 600,
-  //     },
-  //     {
-  //       url: '/opengraph-image.jpg',
-  //       width: 1800,
-  //       height: 1600,
-  //       alt: 'My custom alt',
-  //     },
-  //   ],
-  //   locale: 'en_US',
-  //   type: 'website',
-  // },
+  description: 'Ai tools to answer question of specific context.',
+  openGraph: {
+    title: 'AI : Question answering',
+    description: 'Ai tools to answer question of specific context.',
+    url: 'https://ai-qna-six.vercel.app',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: '/opengraph-image.jpg',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: '/opengraph-image.jpg',
+        width: 1800,
+        height: 1600,
+        alt: 'Ai question answering.',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
