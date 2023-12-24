5194bff718a9a61c19390c0923af0f7d200210af
ashsajal1 <ashsajal1@gmail.com>
2023-12-24 13:25:07 +0800

    Set footer url target to blink

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="flex items-center justify-center w-full fixed md:relative bottom-0 bg-gradient-to-tr from-blue-600 to-violet-600 text-slate-50 p-2 h-[50px]">
-        <p className="text-sm">Developed by <a className="text-slate-100" href="https://github.com/ashsajal1">Ashfiquzzaman Sajal</a></p>
+        <p className="text-sm">Developed by <a className="text-slate-100" href="https://github.com/ashsajal1" target="_blink">Ashfiquzzaman Sajal</a></p>
     </footer>
   )
 }
