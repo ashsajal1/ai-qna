66d30ab602019e626592449eec80f83f3c3d3def
ashsajal1 <ashsajal1@gmail.com>
2023-12-24 17:05:54 +0800

    Redesign and edit cover

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,27 +9,6 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'AI : Question answering',
   description: 'Ai tools to answer question of specific context.',
-  openGraph: {
-    title: 'AI : Question answering',
-    description: 'Ai tools to answer question of specific context.',
-    url: 'https://ai-qna-six.vercel.app',
-    siteName: 'Next.js',
-    images: [
-      {
-        url: '/opengraph-image.jpg',
-        width: 800,
-        height: 600,
-      },
-      {
-        url: '/opengraph-image.jpg',
-        width: 1800,
-        height: 1600,
-        alt: 'Ai question answering.',
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
 }
 
 export default function RootLayout({
