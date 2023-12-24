0ce3302ade0cc63a580b354044dd77bc8cd02a6f
ashsajal1 <ashsajal1@gmail.com>
2023-12-24 16:41:19 +0800

    Add icon and cover

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,27 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'AI : Question answering',
   description: 'Ai tools to answer question of specific context! Api provided by Huggingface.',
+  openGraph: {
+    title: 'AI : Question answering',
+    description: 'Ai tools to answer question of specific context! Api provided by Huggingface.',
+    url: 'https://ai-qna-six.vercel.app',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
