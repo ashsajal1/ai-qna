828caa0b40e361687f86ca52fadf9783dc41de65
ashsajal1 <ashsajal1@gmail.com>
2023-12-24 11:01:45 +0800

    Edit footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,7 @@
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="flex items-center justify-center w-full fixed md:relative bottom-0 bg-gradient-to-tr from-blue-600 to-violet-600 text-slate-50 p-2 h-[30px]">
+        <p className="text-sm">Developed by <a className="text-slate-100" href="https://github.com/ashsajal1">Ashfiquzzaman Sajal</a></p>
+    </footer>
   )
 }
--- src/app/components/Playground.tsx
+++ src/app/components/Playground.tsx
@@ -58,7 +58,7 @@ export default function Playground() {
             </div>
 
 
-            <div className="flex flex-col gap-2 px-12 pt-4 w-full md:w-[650px]">
+            <div className="flex flex-col gap-2 px-12 pt-4 w-full md:w-[650px] mb-[30px] md:mb-0">
                 <textarea className="p-2 h-[300px] outline-none border rounded" value={context} onChange={handleContext} placeholder="Enter Context" />
                 <input className="p-2 outline-none border rounded" value={question} onChange={handleQuestion} type="text" placeholder="Enter question" />
                 <button disabled={isLoadng} className={`p-2 outline-none border rounded bg-gradient-to-tr from-blue-600 to-violet-600 text-slate-50 hover:to-violet-800 ${isLoadng ? "animate-pulse" : ""}`} onClick={handleSubmit}>Get Answer</button>
