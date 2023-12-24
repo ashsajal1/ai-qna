ecbf36d8f8694f0e9eeb083028e9c9aff7aae60b
ashsajal1 <ashsajal1@gmail.com>
2023-12-24 10:33:54 +0800

    Edit the design

--- src/app/components/Playground.tsx
+++ src/app/components/Playground.tsx
@@ -25,12 +25,12 @@ export default function Playground() {
     }
 
     const handleContext = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        console.log(e.target.value)
+        // console.log(e.target.value)
         setContext(e.target.value)
     }
 
     const handleQuestion = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value)
+        // console.log(e.target.value)
         setQuestion(e.target.value)
     }
 
@@ -43,25 +43,25 @@ export default function Playground() {
 
     return (
         <>
-            <h1 className='text-3xl font-bold bg-gradient-to-tr from-blue-600 to-violet-600 text-transparent bg-clip-text'>AI Question answering</h1>
+            <h1 className='text-xl md:text-3xl font-bold bg-gradient-to-tr from-blue-600 to-violet-600 text-transparent bg-clip-text'>AI Question answering</h1>
 
-            <div className={`p-12 w-full ${isLoadng ? "animate-pulse" : ""}`}>
-                <p className="rounded border p-6">
+            <div className={`px-12 pt-4 w-full md:w-[650px] ${isLoadng ? "animate-pulse" : ""}`}>
+                <p className="rounded border border-blue-400 p-6  bg-gradient-to-tr from-blue-600 to-violet-600 text-transparent bg-clip-text">
                     {answer ? (
-                        <p> {isLoadng ? "Hold on! Answering question..." : answer}</p>
+                        <span> {isLoadng ? "Hold on! Answering question..." : answer}</span>
                     ) : (
-                        <p>
+                        <span>
                             {isLoadng ? "Hold on! Answering question..." : "Answer will appear here!"}
-                        </p>
+                        </span>
                     )}
                 </p>
             </div>
 
 
-            <div className="flex flex-col gap-2 p-12 w-full">
-                <textarea className="p-2 outline-none border rounded" value={context} onChange={handleContext} placeholder="Enter Context" />
+            <div className="flex flex-col gap-2 px-12 pt-4 w-full md:w-[650px]">
+                <textarea className="p-2 h-[300px] outline-none border rounded" value={context} onChange={handleContext} placeholder="Enter Context" />
                 <input className="p-2 outline-none border rounded" value={question} onChange={handleQuestion} type="text" placeholder="Enter question" />
-                <button disabled={isLoadng} className={`p-2 outline-none border rounded bg-gradient-to-tr from-blue-600 to-violet-600 text-slate-50 hover:to-violet-800 ${isLoadng ? "opacity-50" : ""}`} onClick={handleSubmit}>Get Answer</button>
+                <button disabled={isLoadng} className={`p-2 outline-none border rounded bg-gradient-to-tr from-blue-600 to-violet-600 text-slate-50 hover:to-violet-800 ${isLoadng ? "animate-pulse" : ""}`} onClick={handleSubmit}>Get Answer</button>
             </div>
         </>
     )
